da4f469cf0597972712ac314ed5ae3bc01f12084
Mark Lamson <lamson_mark@yahoo.com>
2015-06-14 16:52:39 -0400

    added angular-route

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('JavaScript!!!');
+var greenApp = angular.module('greenApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+    
+    $scope.name = 'Main';
+    
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    
+    $scope.num = $routeParams.num || 1;
+    
+}]);
